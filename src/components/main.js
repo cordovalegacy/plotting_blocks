7f36db2fb79602c580bb3075c512043aef17a023
Brendan Cordova <cordovalegacy19@gmail.com>
2022-07-14 19:26:31 -0700

    plotting boxes into page layout using components

---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Sub from './sub_contents';
+
+const Main = () => {
+    return (
+        <div className="main">
+            <Sub />
+            <Sub />
+            <Sub />
+        </div>
+        );
+};
+
+export default Main;
---
+++ src/components/sub_contents.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+    const Sub = () => {
+        return (
+            <div className="sub-content">
+                <p>Sub-content</p>
+            </div>
+            );
+    };
+
+export default Sub;
